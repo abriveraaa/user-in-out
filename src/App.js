d72db05261be8b8994bb651cd7a867d1b7084831
abriveraaa <arielbunagrivera@gmail.com>
2021-02-18 14:49:28 +0800

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Input from './User/Input';
 import Output from './User/Output';
 
-function App() {
+const App = props => {
+  const [ getParagraph, setParagraph ] = useState({
+    paragraph: [
+      { paragraph: "This is a first paragraph." },
+      { paragraph: "This is a second paragraph." }
+    ]
+  }); 
+
+  const switchName = () => {
+    setParagraph( {
+      paragraph: [
+        { paragraph: "This is a 1st paragraph." },
+        { paragraph: "This is a 2nd paragraph." }
+      ]
+    })
+  }
+  
+  const paragraphChangedHandler = (event) => {
+    setParagraph({
+      paragraph: [
+        { paragraph: "Hi! " + event.target.value },
+        { paragraph: "This is a second paragraph." }
+      ]
+    });
+  }
+
+  const style = {
+    fontSize: '20px',
+    margin: '0',
+    textTransform: 'uppercase',
+    letterSpacing: '1px',
+    fontWeight: '500',
+  }
+
   return (
     <div className="App">
-      <Input/>
-      <Output/>
+      <h1 style={style}>User</h1>
+      <Input changed={paragraphChangedHandler}></Input>
+      <div className="button" onClick={switchName}>Click Me!</div>
+      <Output paragraph={getParagraph.paragraph[0].paragraph}></Output>
+      <Output paragraph={getParagraph.paragraph[1].paragraph}></Output>
     </div>
   );
 }
